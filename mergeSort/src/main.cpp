fb8fe4dde04d0f61b2a75dff3b99815d55f402f3
jaichhabra <43894928+jaichhabra@users.noreply.github.com>
2020-10-03 14:45:31 +0530

    Update main.cpp
    newversion for merge sort

--- mergeSort/src/main.cpp
+++ mergeSort/src/main.cpp
@@ -1,52 +1,97 @@
-#include <bits/stdc++.h>
-using namespace std;
+/* C program for Merge Sort */
+#include <stdio.h> 
+#include <stdlib.h> 
 
-template <typename first, typename second>
-std::ostream &operator<<(std::ostream &os, const std::pair<first, second> p)
-{
-    os << p.first << " " << p.second << "";
-    return os;
-}
+// Merges two subarrays of arr[]. 
+// First subarray is arr[l..m] 
+// Second subarray is arr[m+1..r] 
+void merge(int arr[], int l, int m, int r) 
+{ 
+	int i, j, k; 
+	int n1 = m - l + 1; 
+	int n2 = r - m; 
 
-template <typename T>
-std::ostream &operator<<(std::ostream &os, const std::vector<T> v)
-{
-    for (int i = 0; i < v.size(); i++)
-    {
-        os << v[i] << " ";
-    }
-    return os;
-}
+	/* create temp arrays */
+	int L[n1], R[n2]; 
 
-void merge(vector<int> &v, int low, int mid, int high)
-{
-    int n2 = mid - low + 1;
-    int n1 = high - mid;
+	/* Copy data to temp arrays L[] and R[] */
+	for (i = 0; i < n1; i++) 
+		L[i] = arr[l + i]; 
+	for (j = 0; j < n2; j++) 
+		R[j] = arr[m + 1 + j]; 
 
-    vector<int> left(n1);
-    copy(v.begin() + low, v.begin() + mid, left.begin());
-    std::cout << left;
-    vector<int> right(n2 - 1);
-    copy(v.begin() + mid, v.begin() + high, right.begin());
-    std::cout << right;
-}
+	/* Merge the temp arrays back into arr[l..r]*/
+	i = 0; // Initial index of first subarray 
+	j = 0; // Initial index of second subarray 
+	k = l; // Initial index of merged subarray 
+	while (i < n1 && j < n2) { 
+		if (L[i] <= R[j]) { 
+			arr[k] = L[i]; 
+			i++; 
+		} 
+		else { 
+			arr[k] = R[j]; 
+			j++; 
+		} 
+		k++; 
+	} 
 
-void mergeSort(vector<int> &v,int low,int high)
-{
-    if (low < high)
-    {
-        int mid = low + (high - low) / 2;
+	/* Copy the remaining elements of L[], if there 
+	are any */
+	while (i < n1) { 
+		arr[k] = L[i]; 
+		i++; 
+		k++; 
+	} 
 
-        mergeSort(v, low, mid);
-        mergeSort(v, mid + 1, high);
+	/* Copy the remaining elements of R[], if there 
+	are any */
+	while (j < n2) { 
+		arr[k] = R[j]; 
+		j++; 
+		k++; 
+	} 
+} 
 
-        merge(v, low, mid, high);
-    }
-}
+/* l is for left index and r is right index of the 
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r) 
+{ 
+	if (l < r) { 
+		// Same as (l+r)/2, but avoids overflow for 
+		// large l and h 
+		int m = l + (r - l) / 2; 
 
-int main(int argc, char *argv[])
-{
-    vector<int> v = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    mergeSort(v, 0, 10);
-    return 0;
-}
+		// Sort first and second halves 
+		mergeSort(arr, l, m); 
+		mergeSort(arr, m + 1, r); 
+
+		merge(arr, l, m, r); 
+	} 
+} 
+
+/* UTILITY FUNCTIONS */
+/* Function to print an array */
+void printArray(int A[], int size) 
+{ 
+	int i; 
+	for (i = 0; i < size; i++) 
+		printf("%d ", A[i]); 
+	printf("\n"); 
+} 
+
+/* Driver program to test above functions */
+int main() 
+{ 
+	int arr[] = { 12, 11, 13, 5, 6, 7 }; 
+	int arr_size = sizeof(arr) / sizeof(arr[0]); 
+
+	printf("Given array is \n"); 
+	printArray(arr, arr_size); 
+
+	mergeSort(arr, 0, arr_size - 1); 
+
+	printf("\nSorted array is \n"); 
+	printArray(arr, arr_size); 
+	return 0; 
+} 
